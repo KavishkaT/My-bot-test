d6d20a59fd5ecdf6aaaba95223c0c0a6e15d0ae4
KavishkaT <143253480+KavishkaT@users.noreply.github.com>
2024-12-14 04:20:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lp4RhQSB#nFZU6UQ5yqx3h6_iCkgcL-B1SfnayWQ2kJMEDHWbshk",
+SESSION_ID: process.env.SESSION_ID || "Bp4CzKrQ#IJxTFuIkmk0o9h3Gx86Ktj_GkXaiygwHDMgNeDmbgU8",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/pkuujaup.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "true",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
  AUTO_RECORDING: process.env.AUTO_RECORDING || "true",   
 };
